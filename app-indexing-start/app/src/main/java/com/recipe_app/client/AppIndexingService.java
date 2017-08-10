1514f0ffeca5283ebf01806807c536927b5874cd
yuricfurusho <yuricfurusho@livetouch.com.br>
2017-08-10 17:51:16 -0300

    Step 8. Write Personal Content to On-device Index

--- app-indexing-start/app/src/main/java/com/recipe_app/client/AppIndexingService.java
+++ app-indexing-start/app/src/main/java/com/recipe_app/client/AppIndexingService.java
@@ -21,7 +21,28 @@ public class AppIndexingService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        ArrayList<Indexable> indexableNotes = new ArrayList<>();
 
+        for (Recipe recipe : getAllRecipes()) {
+            Note note = recipe.getNote();
+            if (note != null) {
+                Indexable noteToIndex = Indexables.noteDigitalDocumentBuilder()
+                        .setName(recipe.getTitle() + " Note")
+                        .setText(note.getText())
+                        .setUrl(recipe.getNoteUrl())
+                        .build();
+
+                indexableNotes.add(noteToIndex);
+            }
+        }
+
+        if (indexableNotes.size() > 0) {
+            Indexable[] notesArr = new Indexable[indexableNotes.size()];
+            notesArr = indexableNotes.toArray(notesArr);
+
+            // batch insert indexable notes into index
+            FirebaseAppIndex.getInstance().update(notesArr);
+        }
     }
 
     private List<Recipe> getAllRecipes() {
